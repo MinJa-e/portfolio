7a80526c8695699986bdb2aeadac710bcfebf496
codeInJ <80948609+codeInJ@users.noreply.github.com>
2022-01-21 23:55:51 +0900

    모바일 반응형 구현

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -2,9 +2,10 @@ package com.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortfolioApplication {
+public class PortfolioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
